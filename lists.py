f124e4d92c8a22977679f3e3f325af25c7243338
hdjfu <hdjfulucas@gmail.com>
2023-07-18 02:50:23 -0300

    Another

--- lists.py
+++ lists.py
@@ -1,4 +1,19 @@
-a = [1, 2, 3, 4]
-a.append(5)
-a.insert(1 ,7)
-print(a)
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+    
+    def peek(self):
+        last_element = self.stack[len(self.stack)-1]
+        return last_element
+
+stack1 = Stack()
+stack1.push(5)
+stack1.push(3)
+stack1.push(7)
+print(stack1.peek())
